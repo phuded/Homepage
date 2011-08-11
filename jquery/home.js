03cc8d0ecfd1d94438f5a645a651344441754cf4
Matthew Carter <phuded@gmail.com>
2011-08-11 01:17:36 +0100

    Added effect
    Signed-off-by: Matthew Carter <phuded@gmail.com>

--- jquery/home.js
+++ jquery/home.js
@@ -1,28 +1,46 @@
 $(document).ready(function() {
 	/*Fade in content*/
-	$(".page-content").fadeIn();
+	$(".page-content").fadeIn('slow');
 	
 	/*Slides*/
-	$('#slides').slides({
-				preload: true,
-				preloadImage: 'images/slides/loading.gif',
-				play: 5000,
-				pause: 2500,
-				hoverPause: true,
-				animationStart: function(current){
-					$('.caption').animate({
-						bottom:-35
-					},100);
-				},
-				animationComplete: function(current){
-					$('.caption').animate({
-						bottom:0
-					},200);
-				},
-				slidesLoaded: function() {
-					$('.caption').animate({
-						bottom:0
-					},200);
-				}
-	});
+	if ($('#slides').length){
+		$('#slides').slides({
+					preload: true,
+					preloadImage: 'images/slides/loading.gif',
+					play: 5000,
+					pause: 2500,
+					hoverPause: true,
+					animationStart: function(current){
+						$('.caption').animate({
+							bottom:-35
+						},100);
+					},
+					animationComplete: function(current){
+						$('.caption').animate({
+							bottom:0
+						},200);
+					},
+					slidesLoaded: function() {
+						$('.caption').animate({
+							bottom:0
+						},200);
+					}
+		});
+	}
 }); 
+
+$.expand = function(expand, contract, height){
+	
+	$('#'+contract).animate({
+		height: '-='+height,
+	},
+	500,
+	function() {
+		$('#'+expand).animate({
+			height: '+='+height,
+		},
+		500);
+	});
+	
+	$('.show').toggle();
+}
